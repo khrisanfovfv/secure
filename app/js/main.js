29e69bf886ec75b9bcd04448a24dc1d62ac570b3
Fedor Khrisanfov <khrisanfov.fv@gmail.com>
2023-05-14 17:01:51 +0300

    Добавлена карточка контракта

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,28 @@ $(function () {
             href: "card.html"
         })[0].click();
     })
+
+    /** Выбор вкладок на карточке ИС */
+    $('.main_tabs__item').on('click',function(){
+        // Список имеющихся вкладок
+        var card_tabs = ['general','remarks','contracts','archive'];
+
+        // Устанавливаем класс main_tabs__highlighted у выбранной вкладки
+        $('.main_tabs__item').removeClass('main_tabs__highlighted');
+        $(this).addClass('main_tabs__highlighted');
+        
+        /* Скрываем все вкладки */
+        card_tabs.forEach(item => {
+            $('#card__'+ item).addClass('hide_tab');
+        });
+        /* Показываем выбранную */
+        tab=$(this).children().attr('href');
+        $(tab).removeClass('hide_tab');
+    })
+
+
+    /** Клик на строку в таблице Контракты */
+    $('.card__contracts_table tbody tr').on('click',function(){
+        $('.contract').removeClass('hide_tab');
+    })
 })
