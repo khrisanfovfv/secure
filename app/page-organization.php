d9aec43dc6840084a486bc09f31e70445664fff1
dilshoda91 <dilshoda.naimova@mail.ru>
2023-12-19 13:39:39 +0300

    правильная версия 1912

--- app/page-organization.php
+++ app/page-organization.php
@@ -15,40 +15,40 @@ $prefix = $wpdb->prefix;
 
 ?>
 <!-- Идетификатор страницы -->
-<p class="hide" id="page_id">information_system</p>
+<p class="hide" id="page_id">organization</p>
 
 <main class="main">
     <div class="content">
         <div class="reference">
-            <p class="reference__title">Справочник Информационные системы</p>
+            <p class="reference__title">Справочник Организации</p>
             <div class="reference__buttons">
-                <button class="reference__button" id="information_system_create">
+                <button class="reference__button" id="organization_create">
                     <img src="<?php echo $button_icons->create ?>" alt="Создать">
                     <p>Создать</p>
                 </button>
-                <button class="reference__button" id="information_system_edit" disabled>
+                <button class="reference__button" id="organization_edit" disabled>
                     <img src="<?php echo $button_icons->edit ?>" alt="Редактировать">
                     <p>Редактировать</p>
                 </button>
-                <button class="reference__button" id="information_system_copy" disabled>
+                <button class="reference__button" id="organization_copy" disabled>
                     <img src="<?php echo $button_icons->copy ?>" alt="Копировать">
                     <p>Копировать</p>
                 </button>
-                <button class="reference__button" id="information_system_delete" disabled>
+                <button class="reference__button" id="organization_delete" disabled>
                     <img src="<?php echo  $button_icons->delete ?>" alt="Удалить">
                     <p>Удалить</p>
                 </button>
-                <button class="reference__button" id="information_system_excel">
+                <button class="reference__button" id="organization_excel">
                     <img src="<?php echo $button_icons->excel ?>" alt="Excel">
                     <p>Эл. таб</p>
                 </button>
-                <button class="reference__button" id="information_system_update">
+                <button class="reference__button" id="organization_update">
                     <img src="<?php echo $button_icons->update ?>" alt="Update">
                     <p>Обновить</p>
                 </button>
             </div>
-            <div class="information_system__reference_container">
-                <table class="reference__table" id="information_system_table">
+            <div class="organization__reference_container">
+                <table class="reference__table" id="organization_table">
                     <thead>
                         <tr>
                             <th style="width: 35px;">№</th>
@@ -64,13 +64,13 @@ $prefix = $wpdb->prefix;
                         <!-- Выводим строки таблицы -->
                         <?php
                         $rows = $wpdb->get_results(
-                            $wpdb->prepare("SELECT * FROM {$prefix}information_system"),
+                            $wpdb->prepare("SELECT * FROM {$prefix}organization"),
                             ARRAY_A
                         );
                         for ($i = 0; $i < count($rows); $i++) {
                             $row = $rows[$i];
                         ?>
-                            <tr class="information_system_table_row">
+                            <tr class="organization_table_row">
                                 <td class="id hide"><?php echo $row["id"] ?></td>
                                 <td><?php echo $i + 1 ?></td>
                                 <td><?php echo $row["briefname"] ?></td>
@@ -93,29 +93,29 @@ $prefix = $wpdb->prefix;
     </div>
 </main>
 <!-- ДИАЛОГОВОЕ ОКНО -->
-<div class="appdialog" id="information_system_ref__dialog">
-    <div class="appdialog__window" id="information_system_ref__dialog_window">
+<div class="appdialog" id="organization_ref__dialog">
+    <div class="appdialog__window" id="organization_ref__dialog_window">
         <div class="appdialog__header">
-            <h3 class="appdialog__header_title" id="information_system_ref__dialog_title">Карточка Вид документа</h3>
+            <h3 class="appdialog__header_title" id="organization_ref__dialog_title">Карточка Вид документа</h3>
             <a class="appdialog__header_close" href="#">
                 <span>&#10006;</span> </a>
         </div>
         <div class="appdialog__content">
-            <div id="information_system_ref__dialog_content"></div>
+            <div id="organization_ref__dialog_content"></div>
         </div>
     </div>
 </div>
 
 <!-- ОКНО УВЕДОМЛЕНИЯ -->
-<div class="appdialog" id="information_system_ref__notif">
-    <div class="appdialog__window" id="information_system_ref__notif_window">
+<div class="appdialog" id="organization_ref__notif">
+    <div class="appdialog__window" id="organization_ref__notif_window">
         <div class="appdialog__header">
-            <h3 class="appdialog__header_title" id="information_system_ref__notif__header_title">Уведомление</h3>
+            <h3 class="appdialog__header_title" id="organization_ref__notif__header_title">Уведомление</h3>
             <a class="appdialog__header_close" href="#">
                 <span>&#10006;</span> </a>
         </div>
         <div class="appdialog__content">
-            <div id="information_system_ref__notif_content"></div>
+            <div id="organization_ref__notif_content"></div>
             <div class="appdialog__finish_buttons">
                 <button class="appdialog__notify_button">OK</button>
             </div>
@@ -124,17 +124,17 @@ $prefix = $wpdb->prefix;
 </div>
 
 <!-- КОНТЕКТНОЕ МЕНЮ ДЛЯ ТАБЛИЦЫ ИНФОРМАЦИОННЫЕ СИСТЕМЫ -->
-<div class="context-menu" id="information_system_ref__context">
+<div class="context-menu" id="organization_ref__context">
     <ul class="context-menu__list">
-        <li class="context-menu__item" id="information_system_ref__context_edit">
+        <li class="context-menu__item" id="organization_ref__context_edit">
             <img src="<?php echo $button_icons->edit ?>" alt="Редактировать">
             <p>Редактировать</p>
         </li>
-        <li class="context-menu__item" id="information_system_ref__context_copy">
+        <li class="context-menu__item" id="organization_ref__context_copy">
             <img src="<?php echo $button_icons->copy ?>" alt="Копировать">
             <p>Копировать</p>
         </li>
-        <li class="context-menu__item" id="information_system_ref__context_delete">
+        <li class="context-menu__item" id="organization_ref__context_delete">
             <img src="<?php echo  $button_icons->delete ?>" alt="Удалить">
             <p>Удалить</p>
         </li>
@@ -142,13 +142,13 @@ $prefix = $wpdb->prefix;
 </div>
 
 <!-- КОНТЕКСТНОЕ МЕНЮ ЭЛЕМЕНТА reference_content -->
-<div class="context-menu" id="information_system_ref__out_context">
+<div class="context-menu" id="organization_ref__out_context">
 <ul class="context-menu__list">
-    <li class="context-menu__item" id="information_system_ref__out_context_create">
+    <li class="context-menu__item" id="organization_ref__out_context_create">
         <img src="<?php echo $button_icons->create ?>">
         <p>Создать</p>
     </li>
-    <li class="context-menu__item" id="information_system_ref__out_context_update">
+    <li class="context-menu__item" id="organization_ref__out_context_update">
         <img src="<?php echo $button_icons->update ?>">
         <p>Обновить</p>
     </li>
